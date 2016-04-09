1e8ef03765ff13e4313ae5e9cf529a3d61c92279
Lum Naw <lumnaw07@gmail.com>
2016-04-08 19:07:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,23 @@ using namespace std;
 
 int main() {
 
-	string hi = "hi";
-	ToyBox <string> tb;
-	BoxInterface <string> * boxPtr1 = &tb;
-	boxPtr1->setItem(hi);
-	cout << boxPtr1->getItem() << endl;
+    ToyBox <string> tb;
+    MagicBox <string> mb;
+    
+    string input1, input2;
+    
+    BoxInterface <string> * Boxptr1 = &tb;
+    BoxInterface <string>* Boxptr2 =&mb;
+    
+    cout <<"Enter toy to put in toy box: ";
+    cin >> input1;
+    Boxptr1 ->setItem(input1);
+    
+    cout <<"Enter magic to put in magic box: ";
+    cin >> input2;
+    Boxptr2 ->setItem(input2);
+    
+    cout << Boxptr1->getItem() << endl;
+    cout << Boxptr2->getItem() << endl;
 	return 0;
 }
