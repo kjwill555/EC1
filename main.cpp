6d2d55663945267734db56725345d01c5cf3a008
kjwill555 <kjwill555@gmail.com>
2016-04-08 22:12:22 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,23 +9,25 @@ using namespace std;
 
 int main() {
 
-    ToyBox <string> tb;
-    MagicBox <string> mb;
-    
-    string input1, input2;
-    
-    BoxInterface <string> * Boxptr1 = &tb;
-    BoxInterface <string>* Boxptr2 =&mb;
-    
-    cout <<"Enter toy to put in toy box: ";
-    cin >> input1;
-    Boxptr1 ->setItem(input1);
-    
-    cout <<"Enter magic to put in magic box: ";
-    cin >> input2;
-    Boxptr2 ->setItem(input2);
-    
-    cout << Boxptr1->getItem() << endl;
-    cout << Boxptr2->getItem() << endl;
+	ToyBox <string> tb; //ToyBox object
+	MagicBox <string> mb; //MagicBox object
+
+	string input1, input2; //to handle inputs
+
+	BoxInterface <string> * Boxptr1 = &tb; //instantiate using base class pointers
+	BoxInterface <string>* Boxptr2 = &mb;
+
+	//prompt for inputs into each box
+	cout << "Enter toy to put in toy box: ";
+	getline(cin, input1);
+	Boxptr1->setItem(input1);
+
+	cout << "Enter magic to put in magic box: ";
+	getline(cin, input2);
+	Boxptr2->setItem(input2);
+
+	//print the inputs back out
+	cout << Boxptr1->getItem() << endl;
+	cout << Boxptr2->getItem() << endl;
 	return 0;
 }
